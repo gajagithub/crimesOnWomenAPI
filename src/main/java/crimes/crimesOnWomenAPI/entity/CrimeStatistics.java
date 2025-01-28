8c4b5d251552aaa2844253ffef08a3540c1aa78a
gajendirank <gajainbox@gmail.com>
2025-01-29 01:00:03 +0530

    fix runtime error

--- src/main/java/crimes/crimesOnWomenAPI/entity/CrimeStatistics.java
+++ src/main/java/crimes/crimesOnWomenAPI/entity/CrimeStatistics.java
@@ -106,5 +106,6 @@ public class CrimeStatistics {
 
     public CrimeStatistics(int i, String andhraPradesh, int i1, int i2, int i3, int i4, int i5, int i6, int i7, int i8) {
     }
+    public CrimeStatistics(){}
 
     }
