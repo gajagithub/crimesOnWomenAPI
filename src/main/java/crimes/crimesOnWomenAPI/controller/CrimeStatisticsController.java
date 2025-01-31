b8407332e618084eb49b8ab81cbfa85dcb175ee4
gajendirank <gajainbox@gmail.com>
2025-01-31 16:28:13 +0530

    fix runtime error

--- src/main/java/crimes/crimesOnWomenAPI/controller/CrimeStatisticsController.java
+++ src/main/java/crimes/crimesOnWomenAPI/controller/CrimeStatisticsController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/crime-statistics")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CrimeStatisticsController {
 
     @Autowired
